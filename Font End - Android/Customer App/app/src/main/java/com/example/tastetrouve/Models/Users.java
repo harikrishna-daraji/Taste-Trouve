8a0a63fc77039787d38828dee04aba45a00e97c5
sujaykumaryeddu <ysujaykumar@gmail.com>
2021-06-24 12:23:21 -0400

    minor changes

--- Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Models/Users.java
+++ Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Models/Users.java
@@ -14,6 +14,46 @@ public class Users {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void setFcmToken(String fcmToken) {
         this.fcmToken = fcmToken;
     }
