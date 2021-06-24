c5501153c1636a39ec666e163e6175c87ba0be0b
sujaykumaryeddu <ysujaykumar@gmail.com>
2021-06-24 14:09:19 -0400

    Admin Approval

--- Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/ApiInterface.java
+++ Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/ApiInterface.java
@@ -2,11 +2,25 @@ package com.example.tastetrouveadmin;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 
 public interface ApiInterface {
     @GET("restaurantUsers/getRestaurants")
     Call<List<UData>> getUser();
 
+
+
+    @FormUrlEncoded
+    @PUT("restaurantUsers/UpdateRestuarantStatus")
+    Call<ResponseBody> updateRestaurantStatus(
+            @Field("restaurantId") String restaurantId,
+            @Field("updateStatus") String updateStatus
+    );
+
 }
+
