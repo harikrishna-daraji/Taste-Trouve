4b4d331b079e0883051954dbff08f8fc968e649b
Harikrishna <65469425+harikrishna-daraji@users.noreply.github.com>
2021-07-02 19:20:19 -0400

    Map mechanism is completed.

--- Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Activities/MapActivity.java
+++ Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Activities/MapActivity.java
@@ -1,25 +1,94 @@
 package com.example.tastetrouve.Activities;
 
+import static com.google.android.libraries.places.api.Places.createClient;
+import static com.google.android.libraries.places.api.Places.initialize;
+import static com.google.android.libraries.places.api.Places.isInitialized;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.tastetrouve.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Arrays;
+import java.util.List;
+
+public class MapActivity extends BaseActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    String address, city, province, country, postalCode;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        prepareMap();
+        searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Geocoder geocoder = new Geocoder(MapActivity.this);
+                List<Address> list = null;
+                if(!query.isEmpty()) {
+                    try {
+                        list = geocoder.getFromLocationName(query,1);
+                        LatLng latLng = new LatLng(list.get(0).getLatitude(),list.get(0).getLongitude());
+                        String address1 = list.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                        String address2 = list.get(0).getAddressLine(1); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                        city = list.get(0).getLocality();
+                        province = list.get(0).getAdminArea();
+                        country = list.get(0).getCountryName();
+                        postalCode = list.get(0).getPostalCode();
+                        address = address1;
+                        Log.i("TAG","TAG: City: "+city);
+                        Log.i("TAG","TAG: province: "+province);
+                        Log.i("TAG","TAG: country: "+country);
+                        Log.i("TAG","TAG: postalCode: "+postalCode);
+                        Log.i("TAG","TAG: address: "+address);
+
+                        mMap.addMarker(new MarkerOptions().position(latLng));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
+                    } catch (Exception ex) {
+                        Log.i("TAG","TAG "+ex.getMessage());
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void prepareMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
+       mMap = googleMap;
     }
 }
