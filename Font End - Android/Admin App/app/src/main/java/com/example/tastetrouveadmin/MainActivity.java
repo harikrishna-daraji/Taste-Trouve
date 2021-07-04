b0c7ea9c49e94dd1ec8cb0ebccd6e17244a29c40
Gurpreet2010 <gurpreetramgharia470@gmail.com>
2021-07-04 06:12:05 +0100

    admin sign up design done

--- Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/MainActivity.java
+++ Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/MainActivity.java
@@ -56,5 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
     }
 }
---
+++ Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.tastetrouveadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
