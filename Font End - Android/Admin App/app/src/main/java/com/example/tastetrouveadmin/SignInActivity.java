285225e5544986a586931e35438d6f04fe0520f4
Gurpreet2010 <gurpreetramgharia470@gmail.com>
2021-07-05 17:29:40 +0100

    admin app - splash screen design done

--- Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/SignInActivity.java
+++ Font End - Android/Admin App/app/src/main/java/com/example/tastetrouveadmin/SignInActivity.java
@@ -2,9 +2,21 @@ package com.example.tastetrouveadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText email,password;
+    TextView signup;
+    Button signIn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
