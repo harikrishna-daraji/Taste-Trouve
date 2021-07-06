bdfaa8049072117f5455d570573585e452b25e4d
dilroopaz <dilroop@applauz.me>
2021-07-06 19:08:51 -0400

    Add Order Api

--- Backend/routes/Order.js
+++ Backend/routes/Order.js
@@ -1,76 +1,57 @@
 const router = require("express").Router();
 const admin = require("firebase-admin");
 const Product = require("../models/Product");
-const Cart = require("../models/Cart");
+const Order = require("../models/Order");
+const OrderItem = require("../models/OrderItem");
 
 router.post("/add", async (req, res) => {
   try {
-    const { userId, productId, quantity } = req.body;
+    const { userId, addressId, delivery, Products } = req.body;
 
-    const checkOldEntry = await Cart.find({
+    const newOrder = new Order({
       userId,
-      productId,
+      addressId,
+      delivery,
+      orderStatus: "pending",
+      tax,
+      total,
     });
+    const savedOrder = await newOrder.save();
 
-    if (checkOldEntry.length > 0) {
-      var myquery = { _id: checkOldEntry[0]._id };
-      var newvalues = {
-        $set: { quantity: checkOldEntry[0].quantity + parseInt(quantity) },
-      };
-      await Cart.updateOne(myquery, newvalues, function (err, res) {
-        if (err) throw err;
-      });
-      return res.json({ data: "cartUpdated" });
-    } else {
-      const newCart = new Cart({
+    for (var key in Products) {
+      const newOrderItem = new OrderItem({
+        orderId: savedOrder._id,
         userId,
-        productId,
-        quantity,
+        productId: Products[key].productid,
+        quantity: Products[key].quant,
       });
-      const savedCart = await newCart.save();
-      res.json(savedCart);
+      await newOrderItem.save();
     }
+
+    res.json(savedOrder);
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ error: err.message });
   }
 });
 
-router.post("/getCartByUser", async (req, res) => {
-  const { userId } = req.body;
+// router.post("/getCartByUser", async (req, res) => {
+//   const { userId } = req.body;
 
-  const cart = await Cart.find({
-    userId,
-  }).populate("productId");
-  res.json(cart);
-});
+//   const cart = await Cart.find({
+//     userId,
+//   }).populate("productId");
+//   res.json(cart);
+// });
 
-router.post("/updateQuantity", async (req, res) => {
-  const { cartId, quantity } = req.body;
-
-  var myquery = { _id: cartId };
-  var newvalues = {
-    $set: { quantity: parseInt(quantity) },
-  };
-
-  if (parseInt(quantity) == 0) {
-    await Cart.findByIdAndDelete(cartId);
-  } else {
-    await Cart.updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-    });
-  }
-  return res.json({ data: "cartUpdated" });
-});
-
-router.delete("/delete", async (req, res) => {
-  const { cartId } = req.body;
-  try {
-    const deleteCart = await Cart.findByIdAndDelete(cartId);
-    res.json(deleteCart);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// router.delete("/delete", async (req, res) => {
+//   const { cartId } = req.body;
+//   try {
+//     const deleteCart = await Cart.findByIdAndDelete(cartId);
+//     res.json(deleteCart);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 module.exports = router;
