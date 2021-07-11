3181ee74a8c7c652b6694ae092003011ab930595
dilroopaz <dilroop@applauz.me>
2021-07-11 02:31:03 -0400

    Add trackOrder Api + UI

---
+++ Backend/routes/TrackOrder.js
@@ -0,0 +1,61 @@
+//creating the routes
+const router = require("express").Router();
+const TrackOrder = require("../models/TrackOrder");
+// connection for registering a user
+
+router.post("/add", async (req, res) => {
+  try {
+    let { deliveryUser, orderId, lat, long } = req.body;
+
+    const newtrack = new TrackOrder({
+      deliveryUser,
+      orderId,
+      lat,
+      long,
+    });
+
+    const savedtrack = await newtrack.save();
+    res.json(savedtrack);
+  } catch (err) {
+    res.status(500).json({ sporterror: err.message });
+  }
+});
+
+// router.get("/getRestaurants", async (req, res) => {
+//   try {
+//     const restaurant = await Restaurants.find({ status: "pending" });
+
+//     res.send(restaurant);
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+// router.put("/UpdateRestuarantStatus", async (req, res) => {
+//   let { restaurantId, updateStatus } = req.body;
+
+//   Restaurants.updateOne(
+//     { _id: restaurantId },
+//     { status: updateStatus },
+//     function (err, docs) {
+//       if (err) {
+//         res.send(err);
+//       } else {
+//         res.send("Updated");
+//       }
+//     }
+//   );
+// });
+
+// //delete
+// router.delete("/delete", auth, async (req, res) => {
+//   try {
+//     const deletedUser = await Restaurants.findByIdAndDelete(req.user);
+//     res.json(deletedUser);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+module.exports = router;
