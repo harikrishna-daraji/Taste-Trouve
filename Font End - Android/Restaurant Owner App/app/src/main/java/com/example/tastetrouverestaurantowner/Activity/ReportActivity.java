cb0dd714075d87dd4428de143a8db28b570724ef
Gurpreet2010 <gurpreetramgharia470@gmail.com>
2021-07-11 02:44:22 +0100

    minor

--- Font End - Android/Restaurant Owner App/app/src/main/java/com/example/tastetrouverestaurantowner/Activity/ReportActivity.java
+++ Font End - Android/Restaurant Owner App/app/src/main/java/com/example/tastetrouverestaurantowner/Activity/ReportActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tastetrouverestaurantowner.R;
+
 public class ReportActivity extends AppCompatActivity {
 
     @Override
