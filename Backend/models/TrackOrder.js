334f3dbf462cf7391c4ef846f648fbecd4f2c3f4
dilroopaz <dilroop@applauz.me>
2021-07-11 14:51:22 -0400

    fix error

--- Backend/models/TrackOrder.js
+++ Backend/models/TrackOrder.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const trackOrder = new mongoose.Schema({
   deliveryUser: { type: Schema.Types.ObjectId, ref: "DeliveryUser" },
