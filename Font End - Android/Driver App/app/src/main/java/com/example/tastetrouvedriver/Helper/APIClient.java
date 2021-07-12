4c2cb9b0a77c8388de8c806e9ce3b3e18cfffe5a
Harikrishna <65469425+harikrishna-daraji@users.noreply.github.com>
2021-07-12 11:11:08 -0400

    bug fixes

--- Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/APIClient.java
+++ Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/APIClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
 
-    private static final String BASE_URL = "http://192.168.137.1:5000/api/";
+    private static final String BASE_URL = "http://10.0.0.98:5000/api/";
     private static APIClient mInstance;
     private static Retrofit retrofit ;
 
