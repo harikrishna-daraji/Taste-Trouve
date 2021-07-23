289a00c6704af55f568795ba3cdb34f641a182b1
Harikrishna <65469425+harikrishna-daraji@users.noreply.github.com>
2021-07-23 17:07:02 -0400

    conflicts solving of master

--- Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/ApiInterface.java
+++ Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/ApiInterface.java
@@ -37,5 +37,6 @@ public interface ApiInterface {
 
     );
 
+    @FormUrlEncoded
 
 }
--- Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/Fragments/MapFragment.java
+++ Font End - Android/Driver App/app/src/main/java/com/example/tastetrouvedriver/Helper/Fragments/MapFragment.java
@@ -14,6 +14,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+
+
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -25,6 +34,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.activity_map, container, false);
         prepareMap();
+
         return root;
     }
 
@@ -37,4 +47,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
     }
+
+    private void getDriverCurrentOrder() {
+    }
+
 }
