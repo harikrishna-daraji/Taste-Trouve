c870702cd9f28738de1c80b6a70576c361db985c
dilroopaz <dilroop@applauz.me>
2021-08-06 16:52:00 -0400

    implementation of Share products on social media apps

--- Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Activities/ItemDetailsActivity.java
+++ Font End - Android/Customer App/app/src/main/java/com/example/tastetrouve/Activities/ItemDetailsActivity.java
@@ -1,19 +1,31 @@
 package com.example.tastetrouve.Activities;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.telephony.mbms.StreamingServiceInfo;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.tastetrouve.Activities.BaseActivity;
 import com.example.tastetrouve.HelperClass.ApiClient;
 import com.example.tastetrouve.HelperClass.ApiInterface;
@@ -26,6 +38,8 @@ import com.google.android.gms.common.api.Api;
 import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
+
+import java.io.OutputStream;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -40,6 +54,9 @@ public class ItemDetailsActivity extends BaseActivity {
     TextView itemNameTV, kidPriceTV, deliveryTV, calorieTV, descriptionTV, quantityTV, adjustableQuantTV,favouriteText;
     String productId, restaurantId,favoriteValue;
 LinearLayout favoutite;
+   Button share;
+   String shareUrl,shareProductName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +80,7 @@ LinearLayout favoutite;
         adjustableQuantTV = findViewById(R.id.adjustableQuantTV);
         favoutite = findViewById(R.id.favourite);
         favouriteText = findViewById(R.id.favText);
+        share = findViewById(R.id.share);
         favoutite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,6 +103,38 @@ LinearLayout favoutite;
                     quantity = quantity - 1;
                     adjustableQuantTV.setText(String.valueOf(quantity));
                 }
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Glide.with(getApplicationContext())
+                        .asBitmap().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE) .load(shareUrl)
+
+                        .into(new SimpleTarget< Bitmap >(250, 250) {
+                            @Override
+                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+                                Intent intent = new Intent(Intent.ACTION_SEND);
+                                intent.putExtra(Intent.EXTRA_TEXT, shareProductName);
+                                String path = MediaStore.Images.Media.insertImage(getContentResolver(), resource, "", null);
+                                Log.i("quoteswahttodo", "is onresoursereddy" + path);
+
+                                Uri screenshotUri = Uri.parse(path);
+
+                                Log.i("quoteswahttodo", "is onresoursereddy" + screenshotUri);
+
+                                intent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+                                intent.setType("image/*");
+
+                                startActivity(Intent.createChooser(intent, "Share image via..."));
+                            }
+
+
+                        });
             }
         });
 
@@ -169,6 +219,9 @@ LinearLayout favoutite;
 
                 favouriteText.setText(favoriteValue.equals("true")? "Remove from Favourites":"Add to Favourites");
 
+                shareUrl=model.getImage();
+                shareProductName=model.getName();
+
             } else if(getIntent().getStringExtra("type").equals(GlobalObjects.ModelList.Popular.toString())) {
                 PopularSectionModel model = (PopularSectionModel) getIntent().getSerializableExtra("product");
                 Glide.with(this).load(model.getImage()).placeholder(R.drawable.image_placeholder).into(itemImg);
@@ -193,6 +246,8 @@ LinearLayout favoutite;
                         Color.parseColor("#036707") );
 
                 favouriteText.setText(favoriteValue.equals("true")? "Remove from Favourites":"Add to Favourites");
+                shareUrl=model.getImage();
+                shareProductName=model.getName();
 
             } else if(getIntent().getStringExtra("type").equals(GlobalObjects.ModelList.Kid.toString())) {
                 KidSectionModel model = (KidSectionModel) getIntent().getSerializableExtra("product");
@@ -217,6 +272,8 @@ LinearLayout favoutite;
                         Color.parseColor("#036707") );
 
                 favouriteText.setText(favoriteValue.equals("true")? "Remove from Favourites":"Add to Favourites");
+                shareUrl=model.getImage();
+                shareProductName=model.getName();
 
             }
         }
