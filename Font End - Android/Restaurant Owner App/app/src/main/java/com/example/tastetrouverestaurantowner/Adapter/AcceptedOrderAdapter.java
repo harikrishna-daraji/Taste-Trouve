022ce43cd788dd7b618db244052b698effa44455
Harikrishna <65469425+harikrishna-daraji@users.noreply.github.com>
2021-08-09 01:39:40 -0400

    final fixing

--- Font End - Android/Restaurant Owner App/app/src/main/java/com/example/tastetrouverestaurantowner/Adapter/AcceptedOrderAdapter.java
+++ Font End - Android/Restaurant Owner App/app/src/main/java/com/example/tastetrouverestaurantowner/Adapter/AcceptedOrderAdapter.java
@@ -101,16 +101,20 @@ public class AcceptedOrderAdapter extends RecyclerView.Adapter<AcceptedOrderAdap
         orderTotal.setText("$ "+pendingOrderModalArrayList.get(position).getTotalPrice());
         orderDate.setText(pendingOrderModalArrayList.get(position).getOrderDate());
 
-        orderCount.setText(pendingOrderModalArrayList.get(position).getItemCount()+" items");
+        if(pendingOrderModalArrayList.get(position).getItemCount() > 1) {
+            orderCount.setText(pendingOrderModalArrayList.get(position).getItemCount()+" items");
+        } else {
+            orderCount.setText(pendingOrderModalArrayList.get(position).getItemCount()+" item");
+        }
+
 
         address.setText(pendingOrderModalArrayList.get(position).getAddressId().address);
         ratingReview.setText(pendingOrderModalArrayList.get(position).getRatingReview());
         rating.setRating(pendingOrderModalArrayList.get(position).getRatingStar());
 
-        if(pendingOrderModalArrayList.get(0).getRatingReview().equals("") ){
+        if(pendingOrderModalArrayList.get(position).getRatingReview().equals("") ){
             reviewContainer.setVisibility(View.GONE);
         }else {
-
             reviewContainer.setVisibility(View.VISIBLE);
         }
 
